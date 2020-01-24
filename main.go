e13529b22418e087a6dad42033ac6462aa969445
Anastasiya Shaposhnikova <polosate@yandex-team.ru>
2020-01-24 17:22:29 +0300

    some fix

--- main.go
+++ main.go
@@ -70,7 +70,7 @@ func main() {
 	srv := micro.NewService(
 
 		// This name must match the package name given in your protobuf definition
-		micro.Name("steaks.service.product"),
+		micro.Name("product"),
 	)
 
 	// Init will parse the command line flags.
